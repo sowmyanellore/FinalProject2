d92e4023e234502780c44f2ea755caeea3c6416f
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-07 17:22:26 -0500

    updated code

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,51 +1,38 @@
 <!doctype html>
-
 <html lang="en">
+
 <head>
+    
     <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>MY FINALPROJECT</title>
+    <meta name="description" content="MY FINALPROJECT">
     <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
 </head>
+
 
 <body>
 
+   <h1 class="header">Welcome to my application</h1>
+   <p class="links"><a href="index.php?page=accounts&action=all">Show All Accounts</a></p>
+   <p class="links"><a href="index.php?page=tasks&action=all">Show All Tasks</a></p>
 
-<h1>
-    <?php
+   <form action="index.php?page=accounts&action=login" method="POST">
 
-    //this how to print some data;
-    echo $data['site_name'];
+       <div class="input">
+           <label><b>Username</b></label>
+           <input type="text" placeholder="Enter Username" name="username" required>
+       
+           <label><b>Password</b></label>
+           <input type="password" placeholder="Enter Password" name="password" required>
+   
+           <button type="submit"><b>LOGIN</b></button>
+       </div>
 
-    ?> </h1>
+   </form>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+   <p class="links"> Dont have account <a href="index.php?page=accounts&action=register">Register</a></p>
 
-<form action="index.php?page=accounts&action=login" method="POST">
-
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit">Login</button>
-    </div>
-
-
-</form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
-
-
-<script src="js/scripts.js"></script>
 </body>
 </html>
