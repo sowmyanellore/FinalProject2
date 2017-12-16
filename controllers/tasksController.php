03943f95a2ccf8a28eec1a4f9ff7daff80de3df5
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-16 00:22:33 -0500

    added method all

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -1,46 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
 
-
-//each page extends controller and the index.php?page=tasks causes the controller to be called
 class tasksController extends http\controller
 {
-    //each method in the controller is named an action.
-    //to call the show function the url is index.php?page=task&action=show
+    
     public static function show()
     {
         $record = todos::findOne($_REQUEST['id']);
         self::getTemplate('show_task', $record);
     }
 
-    //to call the show function the url is index.php?page=task&action=list_task
+    
 
     public static function all()
     {
-        $records = todos::findAll();
-        /*session_start();
-           if(key_exists('userID',$_SESSION)) {
-               $userID = $_SESSION['userID'];
-           } else {
 
-               echo 'you must be logged in to view tasks';
-           }
+        //$records = todos::findAll();
+	    if(key_exists('userID',$_SESSION)) {
+		   $userID = $_SESSION['userID'];
+	    } else {
+		   header("Location: index.php?page=accounts&action=all");
+	    }
         $userID = $_SESSION['userID'];
 
         $records = todos::findTasksbyID($userID);
-        */
+        
         self::getTemplate('all_tasks', $records);
 
     }
-    //to call the show function the url is called with a post to: index.php?page=task&action=create
-    //this is a function to create new tasks
-
-    //you should check the notes on the project posted in moodle for how to use active record here
 
     public static function create()
     {
