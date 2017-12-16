274115ccd13b46e57dbba78c35418f80c3595835
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-16 15:29:57 -0500

    changed the url  login

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -3,42 +3,45 @@
 class accountsController extends http\controller
 {
 
-    
+    //each method in the controller is named an action.
+    //to call the show function the url is index.php?page=task&action=show
     public static function show()
     {
         $record = accounts::findOne($_REQUEST['id']);
         self::getTemplate('show_account', $record);
     }
 
-    
+    //to call the show function the url is index.php?page=accounts&action=all
 
     public static function all()
     {
-		
+
 		session_start();
 		$userID = @$_SESSION["userID"];
 		if(!$userID) {
 			header("Location: index.php?page=homepage&action=show");
 		}
 		
-        $records = array(accounts::findOne($userID)); 
+        $records = array(accounts::findOne($userID)); //accounts::findAll();
         self::getTemplate('all_accounts', $records);
 
     }
     
     public static function register()
     {
-		
+	
 		session_start();
 		$userID = @$_SESSION["userID"];
 		if($userID) {
 			header("Location: index.php?page=accounts&action=all");
 		}
 		
+        //https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
+        //USE THE ABOVE TO SEE HOW TO USE Bcrypt
         self::getTemplate('register');
     }
 
-    
+    //this is the function to save the user the new user for registration
     public static function store()
 
     {
@@ -53,14 +56,22 @@ class accountsController extends http\controller
             $user->phone = $_POST['phone'];
             $user->birthday = $_POST['birthday'];
             $user->gender = $_POST['gender'];
+            //$user->password = $_POST['password'];
+            //this creates the password
+            //this is a mistake you can fix...
+            //Turn the set password function into a static method on a utility class.
             $user->password = $user->setPassword($_POST['password']);
             $user->save();
 
-            
+            //you may want to send the person to a
+            // login page or create a session and log them in
+            // and then send them to the task list page and a link to create tasks
             header("Location: index.php?page=accounts&action=all");
 
         } else {
-            
+            //You can make a template for errors called error.php
+            // and load the template here with the error you want to show.
+           // echo 'already registered';
             $error = 'already registered';
             self::getTemplate('error', $error);
 
@@ -75,7 +86,7 @@ class accountsController extends http\controller
         self::getTemplate('edit_account', $record);
 
     }
-
+//this is used to save the update form data
     public static function save() {
         $user = accounts::findOne($_REQUEST['id']);
 
@@ -97,9 +108,16 @@ class accountsController extends http\controller
         header("Location: index.php?page=accounts&action=all");
     }
 
+    //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
-        
+        //you will need to fix this so we can find users username.  YOu should add this method findUser to the accounts collection
+        //when you add the method you need to look at my find one, you need to return the user object.
+        //then you need to check the password and create the session if the password matches.
+        //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
+        //after you login you can use the header function to forward the user to a page that displays their tasks.
+        //        $record = accounts::findUser($_POST['email']);
+
         $user = accounts::findUserbyEmail($_REQUEST['email']);
         if($user == FALSE) {
             echo 'user not found';
@@ -110,14 +128,13 @@ class accountsController extends http\controller
                 $_SESSION["userID"] = $user->id;
 
                 //forward the user to the show all todos page
-                header("Location: index.php?page=accounts&action=all");
+                header("Location: index.php?page=tasks&action=all");
             } else {
                 echo 'password does not match';
             }
         }
     }
 	
-
     public static function logout()
     {
         session_start();
