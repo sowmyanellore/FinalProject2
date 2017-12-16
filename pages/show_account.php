d91c8e94bdc99cf9fca0fe5899db5fa70915b21a
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-15 22:02:54 -0500

    addedd back button

--- pages/show_account.php
+++ pages/show_account.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Todo Task Management</title>
+    <meta name="description" content="Todo Task Management">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -16,37 +16,26 @@
 </head>
 
 <body>
-
-<h1>Email: <?php echo $data->email; ?></h1>
-<h1>First Name: <?php echo $data->fname; ?></h1>
-<h1>Last Name: <?php echo $data->lname; ?></h1>
-
-
+<h1>
 <?php
-//this is how you print something  $data contains the record that was selected on the table.
+//this how to print some data;
+$config = Manage::config();
+echo $config['site_name'];
+?></h1>
 
-print_r($data);
+<h2>Show Account</h2>
 
-
-?>
-
-<form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
-
-    First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
-
-    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
-    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
-    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
-    Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
-    Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br>
-    <input type="submit" value="Submit form">
-</form>
-
+<h3>Email: <?php echo $data->email; ?></h3>
+<h3>First Name: <?php echo $data->fname; ?></h3>
+<h3>Last Name: <?php echo $data->lname; ?></h3>
+<h3>Phone: <?php echo $data->phone; ?></h3>
+<h3>Birthday: <?php echo $data->birthday; ?></h3>
+<h3>Gender: <?php echo $data->gender; ?></h3>
 
 <form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
+    <button type="submit" form="form1" value="delete" onClick="return confirm('Are you sure you want to delete this account.');">Delete</button>
 </form>
-
+<a href="index.php?page=accounts&action=all"><button type="button">Back</button></a>
 
 <script src="js/scripts.js"></script>
 </body>
