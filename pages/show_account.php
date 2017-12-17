e52af33829ed530fd1ee8964d8d067d0f310f529
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-17 00:31:29 -0500

    made few changes

--- pages/show_account.php
+++ pages/show_account.php
@@ -4,11 +4,10 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Todo Task Management</title>
-    <meta name="description" content="Todo Task Management">
+    <title>MY PROJECT</title>
+    <meta name="description" content="MY PROJECT">
     <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -16,12 +15,7 @@
 </head>
 
 <body>
-<h1>
-<?php
-//this how to print some data;
-$config = Manage::config();
-echo $config['site_name'];
-?></h1>
+
 
 <h2>Show Account</h2>
 
