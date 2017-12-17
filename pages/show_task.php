d9d433b65776a899bd2a62d184b4016ce812ce1b
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-17 00:32:01 -0500

    made few changes

--- pages/show_task.php
+++ pages/show_task.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>MY PROJECT</title>
+    <meta name="description" content="MY PROJECT">
     <meta name="author" content="SitePoint">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -17,18 +17,35 @@
 
 <body>
 
-<?php
-//this is how you print something  $data contains the record that was selected on the table.
+<h1>Show Todos</h1>
 
-print_r($data);
+<?php
+$userID = $_SESSION["userID"];
+$user_data = accounts::findUserbyId($userID);
 ?>
+
+<h1>Hi <?=$user_data->fname?></h1>
+								
+<ul>
+<li><a href="index.php?page=accounts&action=all">My Account</a>
+</li>
+<li><a href="index.php?page=tasks&action=all">All Todos</a>
+</li>
+<li>
+<a href="index.php?page=accounts&action=logout">Logout</a>
+</li>
+</ul>
+
+
+<h3>Body: <?php echo $data->message; ?></h3>
+<h3>Is Done: <?php echo ($data->isdone == '1'?'Yes':'No'); ?></h3>
+<h3>Create Date: <?php echo $data->createddate; ?></h3>
+<h3>Update Date: <?php echo ($data->updateddate!='0000-00-00'?$data->updateddate:''); ?></h3>
+
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
 </form>
-
-
-
-
+<a href="index.php?page=tasks&action=all">Back</a>
 <script src="js/scripts.js"></script>
 </body>
 </html>
