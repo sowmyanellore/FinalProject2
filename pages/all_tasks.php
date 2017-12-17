c59850980a9e07ddbe80567c3e376f2ff8c66d8c
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-17 14:36:15 -0500

    made slight changes

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Todo Task Management</title>
-    <meta name="description" content="Todo Task Management">
+    <title>MY FINALPROJECT</title>
+    <meta name="description" content="MY FINALPROJECT">
     <meta name="author" content="SitePoint">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+   <link rel="stylesheet" type="text/css" href="style.css">
+    
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -16,22 +17,28 @@
 </head>
 
 <body>
-<h1>
-<?php
-//this how to print some data;
-$config = Manage::config();
-echo $config['site_name'];
-?></h1>
-<h2>All Todos</h2>
 
 
-<ul>
-<li><a href="index.php?page=accounts&action=all">Go Back To My Account</a>
-</li>
 
-<a href="index.php?page=accounts&action=logout">Logout</a>
-</li>
+
+
+<ul >
+<li><a href="index.php?page=tasks&action=create">Create Tasks</a></li>
+
+<li><a href="index.php?page=accounts&action=all">My Account</a></li>
+<li><a  href="index.php?page=accounts&action=logout">Logout</a></li>
 </ul>
+
+
+<h2 class="center">All Todos</h2>
+<h2 style="padding-bottom: 50px;"></h2>
+<?php
+$userID = $_SESSION["userID"];
+$user_data = accounts::findUserbyId($userID);
+?>
+
+<h3>Hii <?=$user_data->fname?>!!!</h3>
+								
 
 <?php
 //this is how you print something
