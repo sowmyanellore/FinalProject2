b72768df84928289a51e2664d78fe171c36f7963
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-12-17 21:11:20 -0500

    final changes with comments

--- collections/accounts.php
+++ collections/accounts.php
@@ -4,14 +4,17 @@ class accounts extends \database\collection
 {
     protected static $modelName = 'account';
 
+    
+  
 
+//This is the function to find user by email for login.
     public static function findUserbyEmail($email)
     {
 
             $tableName = get_called_class();
             $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
 
-         
+         //grab the only record for find one and return as an object
             $recordsSet = self::getResults($sql, $email);
 
             if (is_null($recordsSet)) {
@@ -21,14 +24,14 @@ class accounts extends \database\collection
             }
     }
 	
-	
+	//This is the function to write to find account by user ID for listing on their homepage.
 	public static function findUserbyId($id)
     {
 
             $tableName = get_called_class();
             $sql = 'SELECT * FROM ' . $tableName . ' WHERE id = ?';
 
-        
+         //grab the only record for find one and return as an object
             $recordsSet = self::getResults($sql, $id);
 
             if (is_null($recordsSet)) {
--- collections/todos.php
+++ collections/todos.php
@@ -1,16 +1,20 @@
+
 <?php
 
 class todos extends database\collection
 {
     protected static $modelName = 'todo';
 
-    
+    //This is the function  to find tasks by user ID for listing on their homepage.
+   
     public static  function findTasksbyID($userid) {
 
 
         $tableName = get_called_class();
         $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid = ?';
 
+
+        //grab the only record for find one and return as an object
         $recordsSet = self::getResults($sql, $userid);
 
         if (is_null($recordsSet)) {
